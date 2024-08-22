6c680b7f86e1dc9436758095335fae8841db3024
KAANAPRIYAN <kkaanapriyan@gmail.com>
2024-08-22 19:44:49 +0530

    Included routing

--- client/attendance-app/src/App.js
+++ client/attendance-app/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Login from './Login';
+import Home from './Home';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const isLoggedIn = !!localStorage.getItem('token');
+
+    return (
+        <Router>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/home" element={isLoggedIn ? <Home /> : <Navigate to="/login" />} />
+                <Route path="/" element={<Navigate to={isLoggedIn ? "/home" : "/login"} />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
