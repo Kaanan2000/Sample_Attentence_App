90a77032c242b75db354241900e2eaa436922586
KAANAPRIYAN <kkaanapriyan@gmail.com>
2024-08-22 19:32:09 +0530

    Initialized server code with login API

---
+++ server/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import jwt from 'jsonwebtoken';
+import fs from 'fs';
+
+const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.json());
+
+const users = JSON.parse(fs.readFileSync('users.json'));
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    const user = users.find(u => u.username === username && u.password === password);
+
+    if (user) {
+        const token = jwt.sign({ username }, 'secret_key', { expiresIn: '1h' });
+        res.json({ token });
+    } else {
+        res.status(401).send('Invalid credentials');
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
